04bde339fd21081d63d1df959e44acc90f4cb8fa
Vinicius Fernandes <viniciusfernandes126@gmail.com>
2021-08-25 16:31:21 -0300

    [Requisito 3] Adiciona form com entradas de email e senha

---
+++ script.js
@@ -0,0 +1,20 @@
+function logInValidation() {
+  const logInButton = document.querySelector('#button-header');
+
+  logInButton.addEventListener('click', (event) => {
+    const userEmail = document.querySelector('#input-email').value;
+    const userPassword = document.querySelector('#input-password').value;
+
+    event.preventDefault();
+
+    if (userEmail === 'tryber@teste.com' && userPassword === '123456') {
+      alert('Olá, Tryber!');
+      return;
+    }
+    alert('Email ou senha inválidos.');
+  });
+}
+
+window.onload = () => {
+  logInValidation();
+};
