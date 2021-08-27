87aaddf4db56b7a090a7d1b564873b1c21a8e294
Adson Gomes Oliveira <adsongoliveira2021@gmail.com>
2021-08-27 14:50:51 -0300

    [Projeto Pessoal] CSS alterado e JS alterado

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const submitButton = document.querySelector('#submit-btn');
+const dataContent = document.querySelector('#data-from-form')
 
 function logInValidation() {
   const logInButton = document.querySelector('#trybewarts-button-header');
@@ -49,77 +50,74 @@ function counterText() {
   });
 }
 
-function getText() {
-  const textDiv = document.querySelector('#text-area');
-  const paragraph = document.createElement('p');
-  const retrieveTextArea = document.querySelector('#textarea');
-  const retrieveTextAreaLabel = document.querySelector('.textarea');
-  const retrieveCheckboxAgree = document.querySelector('.checkbox-agreement');
-  const retrieveButton = document.querySelector('#submit-btn');
-  const span = document.querySelector('#counter');
+function changeBackground() {
+  const house = document.querySelector('#house')
+  const main = document.querySelector('main')
 
-  paragraph.innerText = `Observações: ${retrieveTextArea.value}`;
+  house.addEventListener('click', () => {
+    if (house.value === 'Unselected') {
+      main.style.background = 'rgba(0, 0, 0, 0.55) url(images/Castle.webp)'
+    }
 
-  retrieveTextArea.remove();
-  retrieveTextAreaLabel.remove();
-  retrieveCheckboxAgree.remove();
-  retrieveButton.remove();
-  span.remove();
+    if (house.value === 'Gitnória') {
+      main.style.background = 'rgba(0, 0, 0, 0.55) url(images/Red-Castle.jpg)'
+    }
 
-  textDiv.appendChild(paragraph);
+    if (house.value === 'Reactpuff') {
+      main.style.background = 'rgba(0, 0, 0, 0.55) url(images/Blue-Castle.jpg)'
+    }
+
+    if (house.value === 'Corvinode') {
+      main.style.background = 'rgba(0, 0, 0, 0.55) url(images/Green-Castle.jpeg)'
+    }
+
+    if (house.value === 'Pytherina') {
+      main.style.background = 'rgba(0, 0, 0, 0.55) url(images/Yellow-Castle.jpg)'
+    }
+  })
 }
 
 function getName() {
-  const fullName = document.querySelector('#fullname');
   const paragraph = document.createElement('p');
+  const strong = document.createElement('strong');
   const retrieveName = document.querySelector('#input-name');
   const retrieveLast = document.querySelector('#input-lastname');
-  const retrieveLabel1 = document.querySelector('#first');
-  const retrieveLabel2 = document.querySelector('#last')
-  paragraph.innerText = `Nome: ${retrieveName.value} ${retrieveLast.value}`;
 
-  retrieveName.remove();
-  retrieveLabel1.remove();
-  retrieveLast.remove();
-  retrieveLabel2.remove();
+  paragraph.innerText = `${retrieveName.value} ${retrieveLast.value}`;
+  strong.innerText = 'Nome: '
 
-  fullName.appendChild(paragraph);
+  paragraph.insertAdjacentElement('afterbegin', strong)
+  dataContent.appendChild(paragraph)
 }
 
 function getEmail() {
-  const emailDiv = document.querySelector('.email-house');
   const paragraph = document.createElement('p');
+  const strong = document.createElement('strong');
   const retrieveEmail = document.querySelector('#input-email');
-  const label = document.querySelector('#label-email')
 
-  paragraph.innerText = `Email: ${retrieveEmail.value}||`;
+  paragraph.innerText = `${retrieveEmail.value}`;
+  strong.innerText = 'Email: '
 
-  retrieveEmail.remove();
-  label.remove();
-
-  emailDiv.appendChild(paragraph);
+  paragraph.insertAdjacentElement('afterbegin', strong)
+  dataContent.appendChild(paragraph)
 }
 
 function getHouse() {
-  const emailDiv = document.querySelector('.email-house');
   const paragraph = document.createElement('p');
+  const strong = document.createElement('strong');
   const retrieveHouse = document.querySelector('#house');
-  const retrieveHouseLabel = document.querySelector('#house-label');
 
-  paragraph.innerText = `Casa: ${retrieveHouse.value}`;
+  paragraph.innerText = `${retrieveHouse.value}`;
+  strong.innerText = 'Casa: '
 
-  retrieveHouse.remove();
-  retrieveHouseLabel.remove();
-
-  emailDiv.appendChild(paragraph);
+  paragraph.insertAdjacentElement('afterbegin', strong)
+  dataContent.appendChild(paragraph)
 }
 
 function getFamily() {
-  const familyDiv = document.querySelector('#label-family-id');
   const paragraph = document.createElement('p');
+  const strong = document.createElement('strong');
   const retrieveFamily = document.querySelectorAll('.family-class');
-  const retrieveFamilyLabel = document.querySelector('#label-family');
-  const retrieveInternalFamilyDiv = document.querySelector('#radio-family');
   let getUserFamily = '';
 
   for (let index = 0; index < retrieveFamily.length; index += 1) {
@@ -128,19 +126,17 @@ function getFamily() {
     }
   }
 
-  paragraph.innerText = `Família: ${getUserFamily}`;
-  retrieveFamilyLabel.remove();
-  retrieveInternalFamilyDiv.remove();
+  paragraph.innerText = `${getUserFamily}`;
+  strong.innerText = 'Família: '
 
-  familyDiv.appendChild(paragraph);
+  paragraph.insertAdjacentElement('afterbegin', strong)
+  dataContent.appendChild(paragraph)
 }
 
 function getLesson() {
-  const lessonDiv = document.querySelector('.checkbox-content');
   const paragraph = document.createElement('p');
+  const strong = document.createElement('strong');
   const retrieveLessons = document.querySelectorAll('.subject');
-  const retrieveLessonsLabel = document.querySelector('#label-content');
-  const retrieveLessonsCheckbox = document.querySelector('.checkbox-content-value');
   let getUserLessons = '';
 
   for (let index = 0; index < retrieveLessons.length; index += 1) {
@@ -149,18 +145,30 @@ function getLesson() {
     }
   }
   getUserLessons = getUserLessons.replace(',', '');
-  paragraph.innerText = `Matérias: ${getUserLessons}`;
-  retrieveLessonsLabel.remove();
-  retrieveLessonsCheckbox.remove();
-  lessonDiv.appendChild(paragraph);
+
+  paragraph.innerText = `${getUserLessons}`;
+  strong.innerText = 'Matérias: '
+
+  paragraph.insertAdjacentElement('afterbegin', strong)
+  dataContent.appendChild(paragraph)
+}
+
+function getText() {
+  const paragraph = document.createElement('p');
+  const strong = document.createElement('strong');
+  const retrieveTextArea = document.querySelector('#textarea');
+
+  paragraph.innerText = `${retrieveTextArea.value}`;
+  strong.innerText = 'Observações: ';
+
+  paragraph.insertAdjacentElement('afterbegin', strong)
+  dataContent.appendChild(paragraph)
 }
 
 function rating() {
-  const rateDiv = document.querySelector('.radio-family-row');
   const paragraph = document.createElement('p');
+  const strong = document.createElement('strong');
   const retrieveRate = document.querySelectorAll('.rate');
-  const retrieveRateLabel = document.querySelector('#label-rate');
-  const retrieveRateDiv = document.querySelector('#ratings');
   let getUserRate = '';
 
   for (let index = 0; index < retrieveRate.length; index += 1) {
@@ -168,28 +176,123 @@ function rating() {
       getUserRate = retrieveRate[index].value;
     }
   }
-  paragraph.innerText = `Avaliação: ${getUserRate}`;
+  paragraph.innerText = `${getUserRate}`;
+  strong.innerText = 'Avaliação: ';
 
-  retrieveRateLabel.remove();
-  retrieveRateDiv.remove();
+  paragraph.insertAdjacentElement('afterbegin', strong)
+  dataContent.appendChild(paragraph)
+}
 
-  rateDiv.appendChild(paragraph);
+function houseDescription() {
+  const houseSelected = document.querySelector('#house');
+  const division = document.createElement('div')
+
+  division.className = 'division'
+
+  if (houseSelected.value === 'Unselected') {
+    const lore = document.createElement('div');
+    const paragraph = document.createElement('p')
+    const title = document.createElement('h2')
+
+    lore.id = 'lore-house'
+    title.innerText = 'Sem Casa'
+    paragraph.innerText = 'Um mago programador, mesmo que domine diversas magias da programação deve escolher uma casa para servir, um mago sem uma casa é como alguém perdido no deserto, ninguém estenderá a mão para alguém que abandonou as tradições sagradas do código, recarregue a página e escolha uma casa para que os deuses, Giterium, Reactemos, Nodessa, e Pytherina não se enfureçam com você e lhe tirem toda e qualquer habilidade de codar. Escolha sua casa, AGORA!'
+
+    dataContent.appendChild(lore)
+    lore.appendChild(title)
+    lore.appendChild(paragraph)
+    dataContent.appendChild(division)
+  }
+
+  if (houseSelected.value === 'Gitnória') {
+    const lore = document.createElement('div');
+    const paragraph = document.createElement('p')
+    const title = document.createElement('h2')
+
+    dataContent.style.background = 'url(https://i.pinimg.com/originals/70/bd/40/70bd40cfa34632bb52bc1aeebc9dd21e.png)'
+
+    lore.id = 'lore-house'
+    title.innerText = 'Gitnória, A casa dos destemidos programadores!'
+    paragraph.innerText = 'Sua jornada em Gitnória será ardua e está prestes a começar, não pense que será uma colônia de férias, muito pelo contrário, você posto a prova todos os dias enquanto viver no castelo de Gitnória, mas caso seja um developer com garra e determinação o deus Giterium estará sempre de braços abertos para você, assim como a gitnória.'
+
+    dataContent.appendChild(lore)
+    lore.appendChild(title)
+    lore.appendChild(paragraph)
+    dataContent.appendChild(division)
+  }
+
+  if (houseSelected.value === 'Reactpuff') {
+    const lore = document.createElement('div');
+    const paragraph = document.createElement('p')
+    const title = document.createElement('h2')
+
+    dataContent.style.background = 'url(images/react-removebg.png)'
+
+    lore.id = 'lore-house'
+    title.innerText = 'Reactpuff, A casa dos geniais programadores!'
+    paragraph.innerText = 'Sua jornada em Reactpuff será cheia de desafios, apenas os mais inteligentes e ávidos por conhecimento sobrevivem aqui, cada dia será um desafio diferente mais dificil que o anterior está preparado mago programador, a jornada pode ser dificil mas o grande Reactemos lhe dará sabedoria e paz.'
+
+    dataContent.appendChild(lore)
+    lore.appendChild(title)
+    lore.appendChild(paragraph)
+    dataContent.appendChild(division)
+  }
+
+  if (houseSelected.value === 'Corvinode') {
+    const lore = document.createElement('div');
+    const paragraph = document.createElement('p')
+    const title = document.createElement('h2')
+
+    dataContent.style.background = 'url(images/zyro-image.png)'
+
+    lore.id = 'lore-house'
+    title.innerText = 'Corvinode, A casa dos mais eficientes programadores!'
+    paragraph.innerText = 'Antes de escolher Corvinode pense bem, aqui não é lugar de fracos, se não puder lutar pela sua sobrevivência não entre aqui, mas se você tiver a habilidade e força necessária garanto que se tornará o melhor programador que o mundo DEV já viu, casas fortes constroem Devs fortes! Tudo pela graça da grande Nodessa!'
+
+    dataContent.appendChild(lore)
+    lore.appendChild(title)
+    lore.appendChild(paragraph)
+    dataContent.appendChild(division)
+  }
+
+  if (houseSelected.value === 'Pytherina') {
+    const lore = document.createElement('div');
+    const paragraph = document.createElement('p')
+    const title = document.createElement('h2')
+
+    dataContent.style.background = 'url(images/zyro-image1.png)'
+
+    lore.id = 'lore-house'
+    title.innerText = 'Pytherina, A casa dos mais valorosos programadores!'
+    paragraph.innerText = 'A força é importante, a inteligêncai também, assim como a astúcia, porem não podemos esquecer do que nos torna humanos, somos magos programadores, mas ainda temos sentimentos, ainda amamos e odiamos e são essas forças que lhe farão um grande lider e um grande mago programador, sigamos a nossa deusa Pytherina e juntos alcançaremos o topo!'
+
+    dataContent.appendChild(lore)
+    lore.appendChild(title)
+    lore.appendChild(paragraph)
+    dataContent.appendChild(division)
+  }
 }
 
 function generateData() {
+  const sectionData = document.querySelector(".js-control");
+  sectionData.id = 'get-data'
+  houseDescription();
   getName();
   getEmail();
   getHouse();
   getFamily();
   getLesson();
-  rating();
   getText();
+  rating();
 }
 
 function submitBtn() {
   submitButton.addEventListener('click', (event) => {
+    const form = document.querySelector('#evaluation-form')
     event.preventDefault();
+
     generateData();
+    form.remove()
   });
 }
 
@@ -198,4 +301,5 @@ window.onload = () => {
   formsButtom();
   counterText();
   submitBtn();
+  changeBackground()
 };
